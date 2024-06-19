b8e05df67787030c86101cafe2b9f3d09b5556d1
hein-hp <hein.hpq@gmail.com>
2024-06-19 16:21:32 +0800

    feature: 获取表名和表注解

---
+++ src/main/java/cn/hein/handler/TableHandler.java
@@ -0,0 +1,33 @@
+package cn.hein.handler;
+
+import cn.hein.factory.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * 表处理器
+ */
+@Slf4j
+public class TableHandler {
+
+    private static final String SHOW_TABLE_STATUS = "SHOW TABLE STATUS";
+
+    public static void accessTable() {
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(SHOW_TABLE_STATUS);
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            while (resultSet.next()) {
+                String tableName = resultSet.getString("Name"); // 注意MySQL的列名通常大写
+                String comment = resultSet.getString("Comment");
+                log.info("tableName: {}, comment: {}", tableName, comment);
+            }
+        } catch (Exception e) {
+            log.error("Failed to access table", e);
+        }
+    }
+}
